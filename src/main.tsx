bbd8f7873a2355c0650253a253abb19715bd75f4
juan <juansoussev@gmail.com>
2023-08-09 20:48:58 -0300

    Entendendo sobre routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/global.css'
 import { Home } from './components/Home'
+import { About } from './components/About'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    {/* <Home /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
