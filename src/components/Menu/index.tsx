73f0ff40f0f80c1d064a8012a14418ce80fb1f99
juan <juansoussev@gmail.com>
2023-08-09 21:41:58 -0300

    Passando states de uma rota para outra

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,7 +5,7 @@ export const Menu = () => {
   return (
     <nav>
       <ul className='nav'>
-        <li className='nav-item'><Link className='nav-link' to="/">Home</Link></li>
+        <li className='nav-item'><Link className='nav-link' to="/" state={'this is a state from HOME'}>Home</Link></li>
         <li className='nav-item'><Link className='nav-link' to="/about">About</Link></li>
         <li className='nav-item'><Link className='nav-link' to="/posts">Post</Link></li>
         <li className='nav-item'><Link className='nav-link' to="/posts/10">Post 10</Link></li>
