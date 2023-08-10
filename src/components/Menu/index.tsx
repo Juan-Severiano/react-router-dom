ec9d17776abb65cc51955a972106bc507bebd96a
juan <juansoussev@gmail.com>
2023-08-09 21:03:51 -0300

    Trocando os componentes da pagina usadno o Link ao inves de <a></a>

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,13 @@
+import './style.css'
+import { Link } from 'react-router-dom'
+
+export const Menu = () => {
+  return (
+    <nav>
+      <ul className='nav'>
+        <li className='nav-item'><Link className='nav-link' to="/">Home</Link></li>
+        <li className='nav-item'><Link className='nav-link' to="/about">About</Link></li>
+      </ul>
+    </nav>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import './styles/global.css'
 import { Home } from './components/Home'
 import { About } from './components/About'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Menu } from './components/Menu'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <Home /> */}
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
